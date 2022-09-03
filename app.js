4d626f4f3e48e1437066ac6aac10a9974ecf7d09
Sushmita <sushmitakumari363@gmail.com>
2022-09-03 23:54:43 +0530

    style

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var checkBtn=document.querySelector('#check-btn');
 var outputBox=document.querySelector('#output-box');
 
 function compareValues(sum,luckynum){
-    if(sum%luckynum==0){
+    if(sum%luckynum===0){
         outputBox.innerHTML="Your birthday is lucky";
     }
     else
@@ -16,16 +16,26 @@ function compareValues(sum,luckynum){
 function checkBirthdayIsLucky(){
     var dob=dateofbirth.value;
     var sum=calculateSum(dob);
-    compareValues(sum,luckyNumber.value);
+    // console.log(sum);
+    if(sum&&dob){
+        compareValues(sum,luckyNumber.value);
 }
+    else
+    {
+        outputBox.innerHTML="Please Enter valid inputs";
+    }
+    }
+    
 
 function calculateSum(dob)
 {
     dob=dob.replaceAll("-","");
+    // console.log(dob);
     let sum=0;
     for(let i=0;i<dob.length;i++){
-        sum=sum+Number(dob.charAt[i]);
+        sum=sum+Number(dob.charAt(i));
     }
+    // console.log(sum);
     return sum;
 }
 
